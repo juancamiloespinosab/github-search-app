7ddee9ecc248116970cfc97277d9861c5fe44e7b
Juan Espinosa <juancamiloespinosab@gmail.com>
2022-05-12 02:31:44 -0500

    chore: Se crea directorio "modules"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { SearchModule } from './search/search.module';
+import { SearchModule } from '@modules/search/search.module';
 
 @NgModule({
   declarations: [
