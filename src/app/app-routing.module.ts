296732711fa18d9f1c99a886713cdc8ae4fa71c3
Juan Espinosa <juancamiloespinosab@gmail.com>
2022-05-12 02:37:12 -0500

    fix[app-routing]: - Se arregla importación del modulo "Search"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MainComponent } from './search/pages/main/main.component';
-import { SearchComponent } from './search/search.component';
+import { MainComponent } from '@modules/search/pages/main/main.component';
+import { SearchComponent } from '@modules/search/search.component';
 
 const routes: Routes = [
   {
