0f02afa053a0a8516b2fb1069065b39b1592a3a5
Juan Espinosa <juancamiloespinosab@gmail.com>
2022-05-12 02:13:01 -0500

    chore: Se agrega paginación a variables de entorno

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,10 @@ export const environment = {
     baseUrl: "https://api.github.com/search/",
     paths: {
       users: "users"
+    },
+    pagination: {
+      per_page: 20,
+      page: 1
     }
   }
 };
