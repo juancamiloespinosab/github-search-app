5ba0e7b23f6c9aa292a9147ce7221b9794f24025
Juan Espinosa <juancamiloespinosab@gmail.com>
2022-05-15 21:39:31 -0500

    refactor: Se agrega adaptación de la respuesta del API de github

--- src/app/core/interfaces/models/User.tsx
+++ src/app/core/interfaces/models/User.tsx
@@ -2,22 +2,22 @@ export type UserType = 'User' | 'Organization';
 
 export interface User {
     login: string;
-    id: number;
-    node_id: string;
     avatar_url: string;
-    gravatar_id: string;
-    url: string;
-    html_url: string;
-    followers_url: string;
-    following_url: string;
-    gists_url: string;
-    starred_url: string;
-    subscriptions_url: string;
-    organizations_url: string;
-    repos_url: string;
-    events_url: string;
-    received_events_url: string;
     type: UserType;
-    site_admin: boolean;
     score: number;
+    id?: number;
+    node_id?: string;
+    gravatar_id?: string;
+    url?: string;
+    html_url?: string;
+    followers_url?: string;
+    following_url?: string;
+    gists_url?: string;
+    starred_url?: string;
+    subscriptions_url?: string;
+    organizations_url?: string;
+    repos_url?: string;
+    events_url?: string;
+    received_events_url?: string;
+    site_admin?: boolean;
 }
--- src/app/modules/search/services/request.service.ts
+++ src/app/modules/search/services/request.service.ts
@@ -1,11 +1,9 @@
 import { Injectable } from '@angular/core';
 import { User } from '@core/interfaces/models/User';
-import { UsersRequestParameters } from '@core/interfaces/api-request/UsersRequestParameters';
 import { SearchService } from '@core/services/api/search.service';
 import { SharingService } from '@core/services/app/sharing.service';
 import { environment } from 'environments/environment';
-import { UsersResponse } from '@core/interfaces/api-response/UsersResponse';
-import { lastValueFrom } from 'rxjs';
+import { lastValueFrom, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -32,7 +30,18 @@ export class RequestService {
         q: query,
         per_page: this.paginationState.searchUsers.per_page,
         page: this.paginationState.searchUsers.page
-      })
+      }).pipe(
+        map((users: User[]) => {
+          return users.map((user: User) => {
+            return {
+              avatar_url: user.avatar_url,
+              login: user.login,
+              type: user.type,
+              score: user.score
+            }
+          })
+        })
+      )
 
       const users = await lastValueFrom(users$)
       this.sharingService.searchingObservableData = users;
